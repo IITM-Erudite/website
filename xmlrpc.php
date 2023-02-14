f9c709b674a5eaf0e531e0a4daf668cffc91d4d2
Naman Narula <naman@narula.dev>
2023-02-14 12:16:30 +0530

    Add files via upload

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://139.59.19.116/?simply_static_page=268">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://139.59.19.116/?simply_static_page=268";
+		</script>
+
+		<p>You are being redirected to <a href="https://139.59.19.116/?simply_static_page=268">https://139.59.19.116/?simply_static_page=268</a></p>
+	</body>
+</html>
